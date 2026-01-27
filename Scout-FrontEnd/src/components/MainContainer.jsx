8dcec4c5d811f689fa7a74949b34f9f8953514af
UziAlam <u.alam@live.com.au>
2026-01-27 20:07:31 +0100

    up to date

---
+++ Scout-FrontEnd/src/components/MainContainer.jsx
@@ -0,0 +1,14 @@
+function MainContainer() {
+  return (
+    <div>
+        <section className="main-container">
+            <div>
+            </div>
+        
+        </section>
+      
+    </div>
+  );
+}
+
+export default MainContainer;
