f1f0cd1a86ab7c39fb4e198f3e6a98643929d74b
UziAlam <u.alam@live.com.au>
2026-01-29 21:48:02 +0100

    updated version

--- Scout-FrontEnd/src/components/MainContainer.jsx
+++ Scout-FrontEnd/src/components/MainContainer.jsx
@@ -1,13 +1,29 @@
 function MainContainer() {
   return (
-    <div>
-        <section className="main-container">
-            <div>
+      <div className="main-container">
+        <div className="hero-search">
+
+          <div className="what-group">
+            <h2>What</h2>
+
+              <div className="boxes">
+              <input placeholder="Keywords"></input>
+              <input placeholder="Classification"></input>
+              </div>
+              
             </div>
-        
-        </section>
-      
-    </div>
+
+          <div className="where-group">
+            <h2>Where</h2>
+              <div className="boxes">
+                <input placeholder="Enter the city or region"></input> 
+              <button className="search-btn">Scout</button>
+              </div>
+                
+          </div>
+
+          </div>
+      </div>
   );
 }
 
